f448dde20c46026cc87a4200fabcd11ef8bb1a66
daheige <zhuwei313@hotmail.com>
2017-11-05 23:03:26 +0800

    #从模块中导入类

---
+++ 2017/03/Car.py
@@ -0,0 +1,22 @@
+class Car:
+    def __init__(self, name, size, color):
+        self.name = name
+        self.size = size
+        self.color = color
+
+    def resetSize(self, size):
+        self.size = size + 1
+
+    def getDesc(self):
+        print("this car name is" + self.name, "Car color is " + self.name,
+              'size is ' + str(self.size))
+
+class Batt():
+    def __init(self,name,r):
+        self.name = name
+        self.r    = r
+
+    def get_range(self):
+        print("this r is",self.r)
+    
+
---
+++ 2017/03/myCar.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+#从模块中导入类
+from Car import Car
+
+myCar = Car("jipu",12,'green')
+myCar.getDesc()
